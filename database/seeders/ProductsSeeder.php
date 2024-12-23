825842f15579554b1362f3eb0674d3f357f0042e
somonor011 <somonorhong011@gmail.com>
2024-12-23 21:53:43 +0700

    code insert to database

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductsSeeder extends Seeder
 {
@@ -17,9 +18,34 @@ class ProductsSeeder extends Seeder
                 "name" => "iphon 13 pro",
                 "price" => 600,
                 "qty" => 10,
-                "create_at" => now(),
-                "update_at" => now()
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "iphon 11",
+                "price" => 400,
+                "qty" => 5,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Oppo F10",
+                "price" => 500,
+                "qty" => 6,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Xiaomi",
+                "price" => 200,
+                "qty" => 10,
+                "created_at" => now(),
+                "updated_at" => now()
             ]
         ];
+
+        // write code for insert to database
+
+        DB::table("products")->insert($products);
     }
 }
